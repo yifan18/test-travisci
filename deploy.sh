b2bd6f7a674d84f8cc48285fa1664c7f2267d674
qiju_wangjiacheng <qiju_wangjiacheng@qiju.com>
2018-07-06 09:58:51 +0800

    ww

--- deploy.sh
+++ deploy.sh
@@ -32,9 +32,10 @@ rm -rf /home/$temp_folder
 EOF
 
 
+
 # 从latest的package.json里拿到版本号
-if [ -d "$deploy_latest_path/package.json" ]; then
-  last_version=$(cat $deploy_latest_path/package.json | grep version | awk '{print $2}' | sed 's/[",]//g')
+if [ -d "$deploy_latest_path/.version" ]; then
+  last_version=$(cat $deploy_latest_path/.version)
   deploy_last_version_path=$deploy_path/${last_version}
   
 # 创建回滚文件
@@ -42,17 +43,15 @@ cat <<EOF > build/rollback.sh
 # 回滚到上一个版本
 
 # 如果带了版本号参数 直接使用
-custom_version=$1
-if [ $custom_version ] && [ -d $deploy_path/$custom_version ]; then
-  cp -R $deploy_path/$custom_version/ $deploy_latest_path
+custom_version=\$1
+if [ \$custom_version ] && [ -d $deploy_path/\$custom_version ]; then
+  cp -R $deploy_path/\$custom_version/ $deploy_latest_path
   exit
 fi
-
 
 if [ $deploy_last_version_path ]; then
   cp -R $deploy_last_version_path/ $deploy_latest_path
 fi
-# 
 EOF
 fi
 
@@ -62,7 +61,12 @@ fi
 # 把build跟部署文件.sh都传到home下的临时目录
 cp -R "build" "$temp_folder"
 
-# 把临时目录传到部署服务器home目录下
+# 放一个版本文件.version
+cat <<EOF > $temp_folder/build/.version
+PACKAGE_VERSION
+EOF
+
+# 把临时目录传到部署服务器home目录下 这里需要home的权限
 scp -o StrictHostKeyChecking=no -r $temp_folder "$server:/home"
 
 # 执行临时目录下的sdl.sh
